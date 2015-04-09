1264fe574565857107945fe82bcec4620a40f12a
group4mis <ohoodalghaith93@gmail.com>
2015-04-09 10:59:41 +0300

    starting out with github

---
+++ myprojectapp/models.py
@@ -0,0 +1,95 @@
+
+from django.db import models
+
+# Create your models here.
+
+class Account(models.Model):
+
+  user_name = models.CharField(max_length=30,unique=True)
+  type_of_user = models.CharField(max_length=20)
+  password = models.CharField(max_length=30)
+  def __unicode__(self):
+    return self.user_name
+
+class Teacher(models.Model):
+
+  teacher_id = models.CharField(max_length=20,unique=True)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.EmailField(max_length=30)
+  account = models.OneToOneField(Account, null=True, blank=True)
+  def __unicode__(self):
+    return (self.account.user_name , self.teacher_id )
+
+
+class Student(models.Model):
+
+  student_id = models.CharField(max_length=20,unique=True)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.EmailField(max_length=30)
+  account = models.OneToOneField(Account, null=True, blank=True)
+  def __unicode__(self):
+    return (self.account.user_name , self.student_id)
+
+class Parents(models.Model):
+
+  parents_id = models.CharField(max_length=20,unique=True)
+  email = models.EmailField(max_length=30)
+  account = models.OneToOneField(Account, null=True, blank=True)
+  def __unicode__(self):
+    return (self.account.user_name , self.parents_id)
+
+
+class Classes(models.Model):
+
+  class_id = models.CharField(max_length=20,unique=True)
+  class_name = models.CharField(max_length=30,unique=True)
+  class_semester = models.CharField(max_length=30,blank=True)
+  year= models.CharField(max_length=30,blank=True)
+  subject = models.CharField(max_length=30)
+  grade_level = models.IntegerField(max_length=30)
+  classes_teacher= models.ForeignKey(Teacher, blank=True, null=True)
+  def __unicode__(self):
+    return  ( self.classes_teacher.teacher_id , self.class_name)
+
+class Attendance(models.Model):
+  attendance_id = models.CharField(max_length=30,unique=True)
+  student_attendance = models.ForeignKey(Student, blank=True, null=True)
+  classes_attendance = models.ForeignKey(Classes, blank=True, null=True)
+  date = models.DateField()
+  attendancs = models.NullBooleanField()
+
+
+class BehavioralNote(models.Model):
+
+  behavioral_note_id = models.IntegerField(max_length=30,unique=True)
+  date = models.DateField()
+  behavioral_note  = models.TextField(max_length=300,)
+  student_behavioralnote= models.ForeignKey(Student, blank=True, null=True)
+  teacher_behavioralnote = models.ForeignKey(Teacher, blank=True, null=True)
+
+class SpecialNote(models.Model):
+
+  special_note_id = models.IntegerField(max_length=30,unique=True)
+  date = models.DateField()
+  special_note  = models.TextField(max_length=300)
+  classes_specialNote = models.ForeignKey(Classes, blank=True, null=True)
+  teacher_specialNote = models.ForeignKey(Teacher, blank=True, null=True)
+
+class Grade(models.Model):
+
+  grade_id = models.CharField(max_length=30,unique=True)
+  type_g = models.CharField(max_length=30)
+  grade = models.CharField(max_length=30)
+  total_points = models.CharField(max_length=30)
+  student_grade = models.ForeignKey(Student, blank=True, null=True)
+  classes_grade = models.ForeignKey(Classes, blank=True, null=True)
+
+class Meeting(models.Model):
+
+  request_meeting_id = models.IntegerField(max_length=30,unique=True)
+  date = models.DateField()
+  request_meeting  = models.TextField(max_length=300)
+  parents_meeting = models.ForeignKey(Parents,blank=True, null=True)
+  teacher_meeting = models.ForeignKey(Teacher,blank=True, null=True)
