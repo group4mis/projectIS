6fee1d66909e0f1b02a769a5b4cb3d04b004b038
group4mis <ohoodalghaith93@gmail.com>
2015-04-14 12:07:04 +0300

    update login page
    -_-

--- projectis/urls.py
+++ projectis/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'projectis.views.home', name='home'),
+#    url(r'^$', 'projectis.views.base', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/',include('allauth.urls')),
+    url(r'^$', TemplateView.as_view(template_name='base.html'))
 )
