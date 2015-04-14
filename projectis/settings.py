adc0489a9a93074df267a55cabe77e6c427a8c45
group4mis <ohoodalghaith93@gmail.com>
2015-04-14 21:27:12 +0300

    update 2
    -_-

--- projectis/settings.py
+++ projectis/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'joins'
     'myprojectapp',
     'django.contrib.sites',
     'allauth',
