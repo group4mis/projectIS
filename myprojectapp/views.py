3aacc9c5faec8b7dcb1c244128f03f881c5438c5
group4mis <ohoodalghaith93@gmail.com>
2015-04-15 12:57:21 +0300

    update 5 views
    -_-

--- myprojectapp/views.py
+++ myprojectapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
- def base(request):
+def base(request):
 
    return HttpResponseRedirect('/registration')
    return render_to_response("base.html",locals(),context_instance_RequestContext(request))
 
 def registration(request):
 
-  return render_to_response("registration.html",locals(),context_instance_RequestContext(request))
+ return render(request, "projectis/registration.html")
