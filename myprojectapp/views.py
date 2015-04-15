4bd9102719c83e95838c3cf30ce6093d16c25516
Reemalj <reem.jeraidan@gmail.com>
2015-04-15 15:39:22 +0300

    delete space

--- myprojectapp/views.py
+++ myprojectapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
- def base(request):
+def base(request):
    context = {}
    templates = "base.html"
    return render(request, templates, context)
