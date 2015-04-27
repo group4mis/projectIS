b9e38c3222b4906fa140c5d9b8ed5e6b66a3114e
Reemalj <reem.jeraidan@gmail.com>
2015-04-27 10:45:58 +0300

    added attendance template

--- projectis/urls.py
+++ projectis/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^add_classes/$', 'myprojectapp.views.add_classes', name='add_classes'),
     url(r'^delete_classes/$', 'myprojectapp.views.delete_classes', name='delete_classes'),
 
-#    url(r'^grades$','myprojectapp.views.grades',name='grades'),
+#    url(r'^grades/$','myprojectapp.views.grades',name='grades'),
     url(r'^Show_Tmeetings/$','myprojectapp.views.Show_Tmeetings',name='Show_Tmeetings'),
     url(r'^Show_Pmeetings/$','myprojectapp.views.Show_Pmeetings',name='Show_Pmeetings'),
     url(r'^Show_Snotes/$','myprojectapp.views.Show_Snotes',name='Show_Snotes'),
